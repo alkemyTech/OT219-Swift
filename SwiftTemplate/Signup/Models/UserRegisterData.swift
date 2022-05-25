767e1357a528fa7217eadba4384b7cdd3fc3728b
cristian-costa <cristiancosta94@gmail.com>
2022-05-25 19:10:25 -0300

    add models in sign up

---
+++ SwiftTemplate/Signup/Models/UserRegisterData.swift
@@ -0,0 +1,35 @@
+//
+//  UserRegisterData.swift
+//  SwiftTemplate
+//
+//  Created by Cristian Costa on 25/05/2022.
+//
+
+import Foundation
+
+struct UserRegisterData: Codable {
+    let success: Bool
+    let data: UserData
+    let message: String
+}
+
+struct UserData: Codable {
+    let user: UserInfo
+    let token: String
+}
+
+struct UserInfo: Codable {
+    let name, email, password: String
+    let roleId: Int
+    let updatedAt, createdAt: String
+    let id: Int
+}
+
+struct UserRegisterError: Codable {
+    let message: String
+    let errors: Errors
+}
+
+struct Errors: Codable {
+    let email: [String]
+}
---
+++ SwiftTemplate/Signup/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  SwiftTemplate
+//
+//  Created by Cristian Costa on 25/05/2022.
+//
+
+import Foundation
