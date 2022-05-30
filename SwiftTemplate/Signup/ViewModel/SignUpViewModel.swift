d7fab3f188bcb0b493402b30a6d60157264dad63
Agustin Russo <agustin.russo@hotmail.com>
2022-05-29 23:06:43 -0300

    Mail and Password verification functions added

--- SwiftTemplate/Signup/ViewModel/SignUpViewModel.swift
+++ SwiftTemplate/Signup/ViewModel/SignUpViewModel.swift
@@ -23,6 +23,18 @@ class SignUpViewModel {
         } didFail: {
             self.delegate?.userRegisterError()
         }
+        
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+        }
+        
+    func isValidPassword(_ password: String) -> Bool {
+        let passwordRegEx = "[A-Z0-9a-z]"
+        let passwordPred = NSPredicate(format:"SELF MATCHES %@", passwordRegEx)
+        return passwordPred.evaluate(with: password)
+            }
 
     }
 }
