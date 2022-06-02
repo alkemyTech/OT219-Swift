788e38d57ce09808950c88d012167ea20c2356ba
David Duarte <davito.duarte.22@gmail.com>
2022-06-02 05:25:39 -0300

    fix: adding feature/Redirigir-login-signup missing changes

--- SwiftTemplate/Modules/Login/View/LogInViewController.swift
+++ SwiftTemplate/Modules/Login/View/LogInViewController.swift
@@ -21,8 +21,24 @@ class LogInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "LogIn"
         checkIfLogin()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+    }
+   
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    @IBAction func navigateToSignUp(_ sender: Any) {
+        let signUpvc = SignupViewController()
+        self.navigationController?.pushViewController(signUpvc,animated:true)
     }
     
     private func checkIfLogin(){
