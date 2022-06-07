06f13545556f8ff7624b21aaec4b589b1d6feb6c
Cristian Sancricca <cristiansancricca@gmail.com>
2022-06-07 17:59:50 -0300

    nosotros view controller

---
+++ SwiftTemplate/Modules/Nosotros/View/NosotrosViewController.swift
@@ -0,0 +1,196 @@
+//
+//  NosotrosViewController.swift
+//  SwiftTemplate
+//
+//  Created by Cristian Sancricca on 07/06/2022.
+//
+
+import UIKit
+
+class NosotrosViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    private var nosotrosHeader: UILabel = {
+        let label = UILabel()
+        label.text = "¡Nuestro staff!"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    
+    private var logoImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "LOGO-SOMOS MAS")
+        return imageView
+    }()
+    
+    private var profileImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "profilePic")
+        imageView.layer.cornerRadius = 20
+        return imageView
+    }()
+    
+    private var serParteButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = UIColor.white
+        button.setTitle("¡Quiero ser parte!", for: .normal)
+        button.backgroundColor = UIColor(named: "ButtonColor")
+        button.setDimensions(height: 50, width: 200)
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowRadius = 0.0
+        button.layer.masksToBounds = false
+        button.layer.cornerRadius = 10.0
+        return button
+    }()
+    
+    private var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Roberto Martinez"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    
+    private var rolLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Coordinador"
+        label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        label.textColor = .black
+        return label
+    }()
+    
+    private var captionLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: .max))
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique ex massa, sit amet viverra nisi porta eu. Aliquam erat volutpat. Nulla vel aliquet enim. Vivamus aliquet nibh nec magna volutpat, in eleifend libero egestas. Etiam et porttitor tellus. Nam id semper nisl, sit amet egestas velit."
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.sizeToFit()
+        return label
+    }()
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(ProfileCollectionViewCell.self, forCellWithReuseIdentifier: ProfileCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        collectionView.isPagingEnabled = true
+        return collectionView
+    }()
+    
+    lazy var pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.currentPage = 0
+        pageControl.numberOfPages = 10
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        pageControl.addTarget(self, action: #selector(pageControlTapHandler(sender:)), for: .touchUpInside)
+        return pageControl
+    }()
+    
+    private var leftArrowImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "arrow.left.circle.fill")?.withRenderingMode(.alwaysOriginal)
+        image.tintColor = .white
+        image.backgroundColor = .black
+        return image
+    }()
+
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        
+        view.addSubview(logoImage)
+        view.addSubview(nosotrosHeader)
+        view.addSubview(serParteButton)
+        view.addSubview(profileImage)
+        view.addSubview(nameLabel)
+        view.addSubview(rolLabel)
+        view.addSubview(captionLabel)
+        view.addSubview(collectionView)
+        view.addSubview(pageControl)
+        view.addSubview(leftArrowImage)
+        
+        logoImage.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        logoImage.setHeight(90)
+        logoImage.centerX(inView: view)
+        
+        nosotrosHeader.anchor(top: logoImage.bottomAnchor)
+        nosotrosHeader.centerX(inView: view)
+        
+        profileImage.anchor(top: nosotrosHeader.bottomAnchor, paddingTop: 12)
+        profileImage.setDimensions(height: 200, width: 150)
+        profileImage.centerX(inView: view)
+        
+        nameLabel.anchor(top: profileImage.bottomAnchor, left: view.leftAnchor, paddingTop: 12, paddingLeft: 12)
+        
+        rolLabel.anchor(top: nameLabel.bottomAnchor, left: view.leftAnchor, paddingTop: 2, paddingLeft: 12)
+        
+        captionLabel.anchor(top: rolLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 12, paddingLeft: 12, paddingRight: 12)
+        
+        collectionView.anchor(top: captionLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 12, paddingLeft: 12, paddingRight: 12)
+        collectionView.setHeight(100)
+        
+        pageControl.anchor(top: collectionView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor)
+        pageControl.centerX(inView: view)
+        
+        serParteButton.anchor(top: pageControl.bottomAnchor, left: view.leftAnchor, paddingTop: 20, paddingLeft: 12)
+        
+        leftArrowImage.centerY(inView: collectionView)
+        
+    }
+    
+
+    //MARK: - Helpers
+    
+    @objc func pageControlTapHandler(sender: UIPageControl){
+        collectionView.scrollToItem(at: IndexPath(row: sender.currentPage, section: 0), at: .left, animated: true)
+    }
+
+}
+
+extension NosotrosViewController: UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width/4, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProfileCollectionViewCell.identifier, for: indexPath) as? ProfileCollectionViewCell else { return UICollectionViewCell()}
+        
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        pageControl.currentPage = indexPath.row
+    }
+    
+   
+}
