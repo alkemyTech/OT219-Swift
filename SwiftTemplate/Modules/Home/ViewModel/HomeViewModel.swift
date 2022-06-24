5d901fa7f1eb646e5d6370a5a6205952545e14d6
Cristian Sancricca <cristiansancricca@gmail.com>
2022-06-24 00:59:53 -0300

    error general handler

--- SwiftTemplate/Modules/Home/ViewModel/HomeViewModel.swift
+++ SwiftTemplate/Modules/Home/ViewModel/HomeViewModel.swift
@@ -14,7 +14,7 @@ protocol HomeViewModelDelegate: AnyObject {
     func didFailGettingTestimonialsData(error: String)
     func didGetWelcomeData()
     func didFailGettingWelcomeData(error: String)
-    
+    func didFailAllServices()
 }
 
 protocol TimerNewsUpdate: AnyObject {
@@ -32,6 +32,10 @@ class HomeViewModel {
     private var timer : Timer?
     
     private var currentCellIndex = 0
+    
+    private var isServiceNewsAvailable = false
+    //private var isServiceSlidesAvailable = false
+    private var isServiceTestimonialAvailable = false
     
     // MARK: - Welcome methods
     
@@ -62,6 +66,7 @@ class HomeViewModel {
             self?.newsService.fetchNews { news in
                 self?.news = news
                 self?.getNewsCount() == 0 ? self?.delegate?.didFailGettingNewsData(error: ApiError.noNewsData.errorDescription!) : self?.delegate?.didGetNewsData()
+                self?.delegate?.didGetNewsData()
             } onError: { error in
                 self?.delegate?.didFailGettingNewsData(error: error)
             }
@@ -106,6 +111,7 @@ extension HomeViewModel {
             TestimonialsService.shared.fetchTestimonials { [weak self] testimonials in
                 self?.testimonials = testimonials
                 self?.getTestimonialsCount() == 0 ? self?.delegate?.didFailGettingTestimonialsData(error: ApiError.noTestimonialsData.errorDescription!) : self?.delegate?.didGetTestimonialsData()
+                self?.delegate?.didGetTestimonialsData()
             } onError: { [weak self] error in
                 self?.delegate?.didFailGettingTestimonialsData(error: error)
             }
@@ -121,6 +127,54 @@ extension HomeViewModel {
     }
 }
 
+//MARK: - Get all Services
+extension HomeViewModel {
+    
+    
+    func getAllServices() {
+       let dispatchGroup = DispatchGroup()
+       
+        dispatchGroup.enter()
+        newsService.fetchNews { [weak self] news in
+            self?.news = news
+            self?.delegate?.didGetNewsData()
+            self?.isServiceNewsAvailable = true
+            dispatchGroup.leave()
+        } onError: { [weak self] error in
+            self?.isServiceNewsAvailable = false
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        TestimonialsService.shared.fetchTestimonials { [weak self] testimonials in
+            self?.testimonials = testimonials
+            self?.delegate?.didGetTestimonialsData()
+            self?.isServiceTestimonialAvailable = true
+            dispatchGroup.leave()
+        } onError: { [weak self] error in
+            self?.isServiceTestimonialAvailable = false
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.notify(queue: DispatchQueue.global()) {
+            self.checkServiceAvailable()
+        }
+         
+    }
+
+    func checkServiceAvailable()  {
+        if !isServiceNewsAvailable && !isServiceTestimonialAvailable{
+            delegate?.didFailAllServices()
+            return
+        }else if !isServiceNewsAvailable {
+            delegate?.didFailGettingNewsData(error: ApiError.noNewsData.errorDescription!)
+            return
+        } else if !isServiceTestimonialAvailable {
+            delegate?.didFailGettingTestimonialsData(error: ApiError.noTestimonialsData.errorDescription!)
+        }
+    }
+}
+
 //MARK: - Label & Images
 
 struct HomeViewModelLabels {
@@ -132,12 +186,14 @@ struct HomeViewModelLabels {
     static let rolLabelNosotros = "Coordinador"
     static let captionLabelNosotros = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique ex massa, sit amet viverra nisi porta eu. Aliquam erat volutpat. Nulla vel aliquet enim. Vivamus aliquet nibh nec magna volutpat"
     static let testimonialsHeader = "Testimoniales"
+    static let errorMessage = "Oops, something went wrong. Please try again!"
 }
 
 struct HomeViewModelImagesNames {
     static let logoONG = "LOGO-SOMOS MAS"
     static let welcomeImageView = "ong"
     static let profileImageNosotros = "profilePic"
+    static let errorImage = "ErrorIcon"
 }
 
 struct HomeViewModelButtonNames {
@@ -145,4 +201,5 @@ struct HomeViewModelButtonNames {
     static let serParteButtonNews = "¡Quiero ser parte!"
     static let serParteNosotrosButton = "¡Ver todos los miembros!"
     static let verTestimoniosButton = "Ver todos los testimonios"
+    static let errorButton = "Retry?"
 }
